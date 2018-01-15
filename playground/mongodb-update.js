e157e349f9e50ebf99704782b18483feb3bee794
bobrobcpp <king.roob@gmail.com>
2018-01-15 17:50:44 +0000

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,50 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  // db
+  //   .collection('Todos')
+  //   .findOneAndUpdate(
+  //     {
+  //       _id: new ObjectID('5a5cd1662cb31da6cf18c1f3')
+  //     },
+  //     {
+  //       $set: {
+  //         completed: true
+  //       }
+  //     },
+  //     {
+  //       returnOriginal: false
+  //     }
+  //   )
+  //   .then(result => {
+  //     console.log(result);
+  //   });
+
+  db
+    .collection('Users')
+    .findOneAndUpdate(
+      { _id: new ObjectID('5a5c1931cd59a4151c1e029e') },
+      {
+        $set: {
+          name: 'Billy Bob'
+        },
+
+        $inc: {
+          age: 1
+        }
+      },
+      {
+        returnOriginal: false
+      }
+    )
+    .then(result => {
+      console.log(result);
+    });
+
+  // db.close();
+});
